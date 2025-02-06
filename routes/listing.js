a7c5b857bd8ac3956fc8092fc0c78548b3388a56
sahilk <sahil01130@gmail.com>
2025-02-06 15:44:24 +0530

    home route

--- routes/listing.js
+++ routes/listing.js
@@ -11,10 +11,6 @@ const multer = require("multer");
 const { storage, cloudinary } = require("../cloudConfig.js");
 const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } });
 
-router.get("/", (req, res) => {
-  res.redirect("/listings");
-});
-
 router
   .route("/")
   .get(wrapAsync(listingController.index))
